7f88150052a052f57debbd861271ec4f98f20805
laila tabourit <lailatabourit@gmail.com>
2023-06-05 10:35:38 -0700

    ALX_tasks

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <unistd.h>
+/**
+* check_cycle - checks if a singly linked list has a cycle in it.
+*
+* by: laila
+* Return: 0  if there is no cycle, 1 if there is a cycle
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t first_tmp = list;
+	listint_t last_tmp = list;
+
+	while (last_tmp != NULL && first_tmp != NULL && last_tmp->next != NULL)
+	{
+		first_tmp = first_tmp->next;
+		last_tmp = last_tmp->next->next;
+		if(first_tmp == last_tmp)
+			return (1);
+	}
+	return (0);
+}
