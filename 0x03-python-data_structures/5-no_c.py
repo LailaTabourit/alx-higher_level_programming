b7db97c6b9b76cee49f77e6ea65134e63194ce6c
Laila <lailatabourit@gmail.com>
2023-06-11 07:33:07 -0700

    ALX_tasks

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,7 +5,7 @@ def no_c(my_string):
     new_s = my_string[:]
 
     for j in range(leng):
-        if (my_string[j] == 'c' or my_string[n] == 'C'):
+        if (my_string[j] == 'c' or my_string[j] == 'C'):
             new_s = new_s[:(j - i)] + my_string[(j + 1):]
             i += 1
     return (new_s)
