6a795500b93515d8bd09c373715757e445fce11b
Laila <lailatabourit@gmail.com>
2023-06-11 07:10:54 -0700

    ALX_tasks

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    leng = len(my_list)
+
+    if idx < 0 or idx >= leng:
+        return (my_list)
+
+    del my_list[idx]
+    return (my_list)
---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,113 @@
+#include "lists.h"
+/**
+ * reverse - reverses the second half of the list
+ *
+ * @h_r: head of the second half
+ * Return: no return
+ */
+void reverse(listint_t **h_r)
+{
+	listint_t *prev;
+	listint_t *curr;
+	listint_t *nxt;
+
+	prev = NULL;
+	curr = *h_r;
+
+	while (curr != NULL)
+	{
+		nxt = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = nxt;
+	}
+
+	*h_r = prev;
+}
+
+/**
+ * compare - compares each int of the list
+ *
+ * @h1: head of the first half
+ * @h2: head of the second half
+ * Return: 1 if are equals, 0 if not
+ */
+int compare(listint_t *h1, listint_t *h2)
+{
+	listint_t *temp1;
+	listint_t *temp2;
+
+	temp1 = h1;
+	temp2 = h2;
+
+	while (temp1 != NULL && temp2 != NULL)
+	{
+		if (temp1->n == temp2->n)
+		{
+			temp1 = temp1->next;
+			temp2 = temp2->next;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+
+	if (temp1 == NULL && temp2 == NULL)
+	{
+		return (1);
+	}
+
+	return (0);
+}
+
+/**
+ * is_palindrome - checks if a singly linked list
+ * is a palindrome
+ * @head: pointer to head of list
+ * by: laila
+ * Return: 0 if it is not a palindrome,1 if it is a palndrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow, *fast, *prev_slow;
+	listint_t *scn_half, *middle;
+	int isp;
+
+	slow = fast = prev_slow = *head;
+	middle = NULL;
+	isp = 1;
+
+	if (*head != NULL && (*head)->next != NULL)
+	{
+		while (fast != NULL && fast->next != NULL)
+		{
+			fast = fast->next->next;
+			prev_slow = slow;
+			slow = slow->next;
+		}
+
+		if (fast != NULL)
+		{
+			middle = slow;
+			slow = slow->next;
+		}
+
+		scn_half = slow;
+		prev_slow->next = NULL;
+		reverse(&scn_half);
+		isp = compare(*head, scn_half);
+
+		if (middle != NULL)
+		{
+			prev_slow->next = middle;
+			middle->next = scn_half;
+		}
+		else
+		{
+			prev_slow->next = scn_half;
+		}
+	}
+
+	return (isp);
+}
