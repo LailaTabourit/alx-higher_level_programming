2989bda1953c86e3b16233eb6bfa40b6c54e3d4d
Laila <lailatabourit@gmail.com>
2023-06-11 05:24:20 -0700

    ALX_tasks

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,6 @@ def new_in_list(my_list, idx, element):
     leng = len(my_list)
 
     new = my_list[:]
-    if 0 <= idx <leng:
+    if 0 <= idx < leng:
         new[idx] = element
     return (new)
