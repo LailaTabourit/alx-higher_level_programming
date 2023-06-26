65c5d946833fdf10e31977bcddc8d7a6952afde0
Laila <lailatabourit@gmail.com>
2023-06-26 12:04:07 -0700

    ALX_tasks

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,7 +2,7 @@
 def safe_function(fct, *args):
     import sys
     try:
-        result = fct(*args)
+        res = fct(*args)
     except Exception as i:
         sys.stderr.write("Exception: {}\n".format(i))
         res = None
