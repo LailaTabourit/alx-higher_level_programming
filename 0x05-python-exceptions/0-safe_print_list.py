130e3b757b2abf46069c71af98134f92e17b5234
Laila <lailatabourit@gmail.com>
2023-06-26 11:43:56 -0700

    ALX_tasks

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,10 +3,10 @@ def safe_print_list(my_list=[], x=0):
     cmp = 0
     try:
         for i in range(x):
-            print(my_list[i], end ="")
+            print(my_list[i], end="")
             cmp += 1
-        print()
-        return (cmp)
     except IndexError:
+        pass
+    finally:
         print()
-        return (cmp)
+        return cmp
