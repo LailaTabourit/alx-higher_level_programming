47ae09db2d86ae004873bbef4df97ae985c002c3
Laila <lailatabourit@gmail.com>
2023-06-26 11:47:21 -0700

    ALX_tasks

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     cmp = 0
-    try: 
+    try:
         for i in range(x):
             if type(my_list[i]) is int:
                 print("{:d}".format(my_list[i]), end="")
