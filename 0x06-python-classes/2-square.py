bdb60d5e2f45f5b317833eb687b6a038fe87d547
Laila <lailatabourit@gmail.com>
2023-06-27 05:55:33 -0700

    ALX_tasks

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -13,6 +13,6 @@ class Square:
         if type(size) is not int:
             raise TypeError('size must be an integer')
         elif size < 0:
-            raise ValueError('size must be positive ')
+            raise ValueError('size must be >= 0')
         else:
             self.__size = size  #: size of the square
