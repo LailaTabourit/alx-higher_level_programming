bc49c0abcd0de7a47d74437fd0605b9fd57684f6
Laila <lailatabourit@gmail.com>
2023-06-27 04:51:02 -0700

    ALX_tasks

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """class Square defines a square by: size"""
+
+
 class Square:
+    """class Square defines by size"""
     def ___init__(self, size):
         self.__size =  size
