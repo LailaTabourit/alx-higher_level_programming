63e21b5f29c86a7ee7783b3d1c527c488a4d90e3
Laila <lailatabourit@gmail.com>
2023-07-04 11:57:36 -0700

    ALX_tasks

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+
+Module 2-matrix_divided.py
+
+"""
+
+def matrix_devided(matrix, div):
+    """ Function  that divides all elements of a matrix.
+
+    Args:
+        matrix: list of a lists of numbers
+        div: number which divides the matrix
+
+    Returns:
+        new matrix
+
+    Raises:
+        TypeError: If the elements of the matrix aren't lists
+                   If the elemetns of the lists aren't integers/floats
+                   If div is not an integer/float number
+                   If the lists of the matrix don't have the same size
+
+        ZeroDivisionError: If div is zero
+
+    """
+    if not type(div) in (int, float):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    msg_type = "matrix must be a matrix (list of lists) of integers/floats"
+
+    if not matrix or not isinstance(matrix, list):
+        raise TypeError(msg_type)
+
+    len_e = 0
+    msg_size = "Each row of the matrix must have the same size"
+
+    for elems in matrix:
+        if not elems or not isinstance(elems, list):
+            raise TypeError(msg_type)
+
+        if len_e != 0 and len(elems) != len_e:
+            raise TypeError(msg_size)
+
+        for num in elems:
+            if not type(num) in (int, float):
+                raise TypeError(msg_type)
+
+        len_e = len(elems)
+
+    n = list(map(lambda x: list(map(lambda y: round(y / div, 2), x)), matrix))
+    return (n)
+
