3129c1710084094bfbf530b07a244a5f9ad1e126
Laila <lailatabourit@gmail.com>
2023-07-10 05:36:53 -0700

    ALX_tasks

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!?usr/bin/python3
+"""class MyList that inherits from list"""
+
+
+class Mylist(list):
+    """class that inherits from list"""
+    def print_sorted(self):
+        """that prints the list, but sorted
+        (ascending sort)
+        """
+        print(sorted(self))
