3e279ca73afbcd95211a914b7ebf00d7a522d103
Laila <lailatabourit@gmail.com>
2023-07-10 05:56:11 -0700

    ALX_tasks

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""function that returns True if the object is an instance of a class that inherited
+ (directly or indirectly) from the specified class ;
+ otherwise False.
+"""
+
+
+def inherits_from(obj, a_class):
+    """returns True if the object is an instance of a class
+       that inherited (directly or indirectly) from the specified class;
+       otherwise False.
+    """
+    if type(obj) != a_class:
+        return issubclass(type(obj), a_class)
+    else:
+        return False
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module base geometry (based on 5-base_geometry.py)
+"""
+
+
+class BaseGeometry:
+    """ empty class"""
+    def area(self):
+        raise Exception('area() is not implemented')
