f3acf7866deb6f3a3fbf9cb8735382ea3f5a38ae
Laila <lailatabourit@gmail.com>
2023-07-10 06:03:28 -0700

    ALX_tasks

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""function that returns True if the object is an instance of a class that inherited
- (directly or indirectly) from the specified class ;
- otherwise False.
-"""
+"""function only sub class"""
 
 
 def inherits_from(obj, a_class):
