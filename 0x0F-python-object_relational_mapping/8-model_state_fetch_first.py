6f302ad5affa1968962da437ae0e339e4d845ad0
Laila <lailatabourit@gmail.com>
2023-09-16 11:23:35 -0700

    ALX_tasks

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Prints the first State object from the database hbtn_0e_6_usa"""
+""" Prints the first State object from the database hbtn_0e_6_usa
+"""
 import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
